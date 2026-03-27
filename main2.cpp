74c3623f5d63a7ecb08ccaa424d5166baee6d2ef
ThomasRhoden <rhodenthomas@gmail.com>
2026-03-27 20:22:20 -0300

    test2

---
+++ main2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+
+int main(){
+
+    std::cout<<8+2<<std::endl;
+    std::cout<<10-4<<std::endl;
+    std::cout<<4*3<<std::endl;
+    std::cout<<8/2<<std::endl;
+
+    return 0;
+}
