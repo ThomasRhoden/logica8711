e38df65d7823c0ab6f5bd1dc596c45fb49024063
ThomasRhoden <rhodenthomas@gmail.com>
2026-03-27 20:32:31 -0300

    test3

---
+++ main4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+
+int main(){
+
+    int num1 = 8;
+    int num2 = 2;
+    int soma, subt, mult, div;
+    
+    soma = num1 + num2;
+    subt = num1 - num2;
+    mult = num1 * num2;
+    div = num1 / num2;
+
+    std::cout<<soma<<std::endl;
+    std::cout<<subt<<std::endl;
+    std::cout<<mult<<std::endl;
+    std::cout<<div<<std::endl;
+
+    return 0;
+}
