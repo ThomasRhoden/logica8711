2365992ea6be11a7e3fa3c2c3569055e6497cff6
ThomasRhoden <rhodenthomas@gmail.com>
2026-03-27 21:09:23 -0300

    test4

---
+++ main5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+
+int main(){
+
+    int idade = 31;
+    float altura = 1.80;
+    std::string nome = "Thomas";
+
+
+
+
+    std::cout<<"Meu nome é "<<nome<<"."<<std::endl;
+    std::cout<<"Minha altura é "<<altura<<"m."<<std::endl;
+    std::cout<<"Minha idade é "<<idade<<" anos."<<std::endl;
+    
+
+    return 0;
+}
