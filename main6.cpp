99814e588a8be3f82d77c6797c116e3efa9d060c
ThomasRhoden <rhodenthomas@gmail.com>
2026-03-27 21:27:40 -0300

    test6

---
+++ main6.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+
+int main(){
+
+    std::string nome;
+
+    std::cout<<"Qual seu nome: "<<std::endl;
+    std::cin>>nome;
+
+    std::cout<<"Bem-vindo(a) "<<nome<<"!!"<<std::endl;
+
+
+
+    return 0;
+}
